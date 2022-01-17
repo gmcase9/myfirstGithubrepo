e342ef84d71024350934e340b7dea74e9e1ea9b9
Graham <89858346+gmcase9@users.noreply.github.com>
2022-01-17 16:11:31 -0500

    first commit
    this is my first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Hello there! What is your name?");
+var lastName = prompt("Hello there! What is your last name?");
+
+document.body.querySelector(".firstName").innerHTML=firstName;
+document.body.querySelector(".lastName").innerHTML=lastName;
